19477c2d741346fbe11fdfc0068f7268dac0ecf5
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-17 17:17:57 -0300

    primeng card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,14 @@ export class AppComponent {
 
   nombre: string = 'eric denis';
   valor: number = 12345;
+  obj = {
+    nombre: 'Eric'
+  };
 
   mostrarNombre() {
     console.log(this.nombre)
+    console.log(this.valor)
+    console.log(this.obj)
   }
 
 }
