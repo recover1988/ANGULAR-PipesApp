c30e0c87fb8f441abe2461f912fc7037d02729ad
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-17 16:50:48 -0300

    PrimerNg instalado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string = 'eric denis';
+  valor: number = 12345;
+
+  mostrarNombre() {
+    console.log(this.nombre)
+  }
+
 }
